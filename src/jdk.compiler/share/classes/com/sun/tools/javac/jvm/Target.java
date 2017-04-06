7afb2dd47b2e3031398913549292d5ded423197f
Liam Miller-Cushon <cushon@google.com>
2017-04-05 17:58:24 -0700

    Default to source/target 8

--- src/jdk.compiler/share/classes/com/sun/tools/javac/jvm/Target.java
+++ src/jdk.compiler/share/classes/com/sun/tools/javac/jvm/Target.java
@@ -106,7 +106,7 @@ public enum Target {
         this.minorVersion = minorVersion;
     }
 
-    public static final Target DEFAULT = values()[values().length - 1];
+    public static final Target DEFAULT = JDK1_8;
 
     public static Target lookup(String name) {
         return tab.get(name);
